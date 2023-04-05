09d51c2f52eb1818e46d310e945928c819c06970
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-06 01:37:28 +0700

    adj split and join length

--- plugins/community.lua
+++ plugins/community.lua
@@ -12,8 +12,15 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
-  { import = "astrocommunity.editing-support.treej" },
+  { import = "astrocommunity.editing-support.treej"},
   { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.syntax.hlargs-nvim" },
+  {
+    "treesj",
+    opts = {
+      max_join_length = 240,
+      max_split_length = 240,
+    }, 
+  },
 }
