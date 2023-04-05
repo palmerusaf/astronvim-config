8c080ebd9bf32b34d61ffe5cd7412809795242b0
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-06 01:04:41 +0700

    add community plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,4 +7,13 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.treej" },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
+  { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.syntax.hlargs-nvim" },
 }
