25dc92842c64f9f8ab23bcaf7a569143fdce4a98
Branden Palmer <brandenpalmer08@gmail.com>
2023-04-11 14:50:59 +0700

    switch to default theme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "onedark",
+  colorscheme = "astrotheme",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
