13a351e60b1ed862608018373631677311f11e1a
Micah Halter <micah@mehalter.com>
2023-04-28 09:56:43 -0400

    fix(plugins): protect ensure_installed table from community packs

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,7 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
+  opts = function(_, opts)
+    -- add more things to the ensure_installed table protecting against community packs modifying it
+    if not opts.ensure_installed then opts.ensure_installed = {} end
+    require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "bash",
       "css",
       "gitignore",
@@ -15,6 +17,6 @@ return {
       "tsx",
       "typescript",
       "yaml",
-    },
-  },
+    })
+  end,
 }
