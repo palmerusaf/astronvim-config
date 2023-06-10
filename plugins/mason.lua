fbc83eb99de609f5f85b5a7e50b4ad25ed74e4ce
Branden Palmer <brandenpalmer08@gmail.com>
2023-06-10 13:23:25 +0700

    ensure lsps and formatters are installed

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -5,7 +5,16 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = {
+        "lua_ls",
+        "html",
+        "cssls",
+        "bashls",
+        "jsonls",
+        "marksman",
+        "tailwindcss",
+        "tsserver",
+      },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,7 +22,11 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = {
+        "prettierd",
+        "rustywind",
+        "stylua",
+      },
     },
   },
   {
