f6df25580cb9b613e3275aebe68057a2f7376c0a
Branden Palmer <brandenpalmer08@gmail.com>
2023-07-01 10:36:06 +0700

    add flit after astro community removed it from leap plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,12 +7,13 @@ return {
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.flit-nvim" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.markdown" },
   -- { import = "astrocommunity.editing-support.refactoring-nvim" },
-  { import = "astrocommunity.editing-support.treej"},
+  { import = "astrocommunity.editing-support.treej" },
   { import = "astrocommunity.editing-support.nvim-ts-rainbow2" },
   { import = "astrocommunity.git.git-blame-nvim" },
   { import = "astrocommunity.syntax.hlargs-nvim" },
@@ -21,6 +22,6 @@ return {
     opts = {
       max_join_length = 240,
       max_split_length = 240,
-    }, 
+    },
   },
 }
